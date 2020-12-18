9c815c7fbca1824a06c01bd58ca4be5ad6edb2d3
RL2020 <loc.fitness1@gmail.com>
2020-12-18 10:55:03 -0500

    commit tree 8d3c9547342ea57e7bc55d8c1030e30b560655b5

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name 
+    end 
+
+    def grade(num)
+        roster[num]
+    end 
+
+    def sort
+        student = {}
+        roster.each { |grade, name| student[grade] = name.sort}
+        student
+    end 
+end 
